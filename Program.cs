bd3c1d727eb6e37bb299f6372af7c48281e2235f
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-21 19:48:09 +0300

    Create Program.cs
    DZ

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            float water;
+            bool howmuchMoney;
+            double numberN;
+            char chat;
+            string name;
+            long anExperience;
+            byte century;
+            ulong map;
+            short armor;
+        }
+    }
+}
